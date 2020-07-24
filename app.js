eed411e5f9695f2f888f5373654b1b41319858d3
Chiang <hengjenchiang@gmail.com>
2020-07-24 19:12:20 +0800

    Add all files

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res)=>{
+	res.render("home");
+});
+
+app.get("/about", (req, res)=>{
+	res.render("about");
+});
+
+app.listen(3000, (req, res)=>{
+	console.log("started demo deploying");
+});
